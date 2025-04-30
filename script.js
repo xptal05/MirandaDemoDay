6f5b4288d40d52a88faea073178a2c281d88aee4
Lucie Lanneree <ptackovalucy@gmail.com>
2025-04-30 14:37:29 +0200

    Style sheet adjustments and minor js adjustments

--- script.js
+++ script.js
@@ -31,6 +31,16 @@ async function fetchproducts() {
         return '';
       }).join('');
 
+      const availabilityMap = {
+        "Skladem": "green",
+        "Na objednávku": "grey",
+        "Momentálně nedostupné": "red"
+      };
+      
+      const statusClass = availabilityMap[product.availability] || "";
+      const formattedPrice = new Intl.NumberFormat('cs-CZ').format(product.price);
+
+    
       productDiv.innerHTML = `
 
           <div class="product-img-wrapper">
@@ -40,10 +50,10 @@ async function fetchproducts() {
           <div class="product-title">${product.title}</div>
           <div class="product-info-wrapper">
             <div>
-                <div class="product-stock-status">${product.availability}</div>
-                <div class="product-price">${product.price} Kč</div>
+                <div class="product-stock-status ${statusClass}">${product.availability}</div>
+                <div class="product-price">${formattedPrice} CZK</div>
             </div>
-            <div class="add-to-card-btn"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 22a1 1 0 1 0 0-2 1 1 0 0 0 0 2m11 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></div>
+            <button class="add-to-card-btn"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 22a1 1 0 1 0 0-2 1 1 0 0 0 0 2m11 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></button>
           </div>
 
       `;
